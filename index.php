2ab93e0924ffeb8e8db62d01f2b5fa7fb725d4f4
Nikola TS <nikola.jovanovic@thingsolver.com>
2021-12-24 14:23:14 +0100

    Ugradjen navbar

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
+    <title>Televizijski program</title>
+    
+        <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="css/style.css" type="text/css"> 
+
+
+</head>
+<body>
+    
+
+<header class="header">
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">TV PROGRAM</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      
+      <li><a href="#">PRETRAŽI </a></li>
+      <li><a href="#">UNESI</a></li>
+      <li><a href="#">PROGRAMSKA ŠEMA</a></li>
+    </ul>
+  </div>
+</nav>
+</header>
+
+<div class="txt1"> nnnnn</div>
+</body>
+</html>
